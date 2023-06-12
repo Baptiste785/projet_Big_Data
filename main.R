ad1b0cb6f70d738811f39737664eeb9fadfe4e7a
Baptiste785 <baptiste.bockler@isen-ouest.yncrea.fr>
2023-06-12 17:26:45 +0200

    Update main.R
    dernière modif avant de partir

--- main.R
+++ main.R
@@ -19,13 +19,53 @@ data$descr_grav
 
 ##changement de donnée pour l'agglomération ou non 
 for (i in 1:73643){
-  if(data$descr_grav[i]=="Hors agglomÃ©ration"){
-    data$descr_grav[i] <- 0;
+  if(data$descr_agglo[i]=="Hors agglomÃ©ration"){
+    data$descr_agglo[i] <- 0;
   }
-  if(data$descr_grav[i]=="En agglomÃ©ration"){
-    data$descr_grav[i] <- 1;
+  if(data$descr_agglo[i]=="En agglomÃ©ration"){
+    data$descr_agglo[i] <- 1;
   }
 }
 
+##changemenent de donnée athmoshéprique 
+for (i in 1:73643){
+  if(data$descr_athmo[i]=="Vent fort â€“ tempÃªte"){
+    data$descr_athmo[i] <- 0;
+  }
+  if(data$descr_athmo[i]=="Temps couvert"){
+    data$descr_athmo[i] <- 1;
+  }
+  if(data$descr_athmo[i]=="Temps Ã©blouissant"){
+    data$descr_athmo[i] <- 2;
+  }
+  if(data$descr_athmo[i]=="Pluie lÃ©gÃ¨re"){
+    data$descr_athmo[i] <- 3;
+  }
+  if(data$descr_athmo[i]=="Normale"){
+    data$descr_athmo[i] <- 4;
+  }
+  if(data$descr_athmo[i]=="Brouillard â€“ fumÃ©e"){
+    data$descr_athmo[i] <- 5;
+  }
+  
+  if(data$descr_athmo[i]=="Autre"){
+    data$descr_athmo[i] <- 6;
+  }
+  
+}
+
+
 ##changement de donnée pour l'agglomération ou non 
+for (i in 1:73643){
+  if(data$descr_lum[i]=="CrÃ©puscule ou aube"){
+    data$descr_lum[i] <- 0;
+  }
+  if(data$descr_lum[i]=="Nuit avec Ã©clairage public allumÃ©"){
+    data$descr_lum[i] <- 1;
+  }
+  if(data$descr_lum[i]=="Plein jour"){
+    data$descr_lum[i] <- 2;
+  }
+}
+
 
