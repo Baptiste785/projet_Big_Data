8af4810d5ceb31a818544f3cb6f68efb5a68b85f
Baptiste785 <baptiste.bockler@isen-ouest.yncrea.fr>
2023-06-13 11:29:28 +0200

    Update main.R
    description

--- main.R
+++ main.R
@@ -1,242 +1,254 @@
 data = stat_acc_V3
 
-##changement de donnée pour la descritpion de la personne après l'accident
-  for (i in 1:73643){
-    if(data$descr_grav[i]=="Indemne"){
-      data$descr_grav[i] <- 0;
-    }
-    if(data$descr_grav[i]=="BlessÃ© hospitalisÃ©"){
-      data$descr_grav[i] <- 1;
-    }
-    if(data$descr_grav[i]=="TuÃ©"){
-      data$descr_grav[i] <- 2;
-    }
-    if(data$descr_grav[i]=="BlessÃ© lÃ©ger"){
-      data$descr_grav[i] <- 3;
-    }
-  }
 
-##changement de donnée pour l'agglomération ou non 
-for (i in 1:73643){
-  if(data$descr_agglo[i]=="Hors agglomÃ©ration"){
-    data$descr_agglo[i] <- 0;
-  }
-  if(data$descr_agglo[i]=="En agglomÃ©ration"){
-    data$descr_agglo[i] <- 1;
-  }
-}
+##changement de donnée pour les catégories de véhicule
+data$descr_cat_veh<-factor(data$descr_cat_veh)
+data$descr_cat_veh<-as.numeric(data$descr_cat_veh)
 
-##changemenent de donnée athmoshéprique 
-for (i in 1:73643){
-  if(data$descr_athmo[i]=="Vent fort â€“ tempÃªte"){
-    data$descr_athmo[i] <- 0;
-  }
-  if(data$descr_athmo[i]=="Temps couvert"){
-    data$descr_athmo[i] <- 1;
-  }
-  if(data$descr_athmo[i]=="Temps Ã©blouissant"){
-    data$descr_athmo[i] <- 2;
-  }
-  if(data$descr_athmo[i]=="Pluie lÃ©gÃ¨re"){
-    data$descr_athmo[i] <- 3;
-  }
-  if(data$descr_athmo[i]=="Normale"){
-    data$descr_athmo[i] <- 4;
-  }
-  if(data$descr_athmo[i]=="Brouillard â€“ fumÃ©e"){
-    data$descr_athmo[i] <- 5;
-  }
-  
-  if(data$descr_athmo[i]=="Autre"){
-    data$descr_athmo[i] <- 6;
-  }
-  if(data$descr_athmo[i]=="Pluie forte"){
-    data$descr_athmo[i] <- 7;
-  }
-  
-  if(data$descr_athmo[i]=="Neige â€“ grÃªle"){
-    data$descr_athmo[i] <- 8;
-  }
-  
-}
+
+##changement de donnée pour l'agglomération ou non
+data$descr_agglo<-factor(data$descr_agglo)
+data$descr_agglo<-as.numeric(data$descr_agglo)
+
+
+##changemenent de donnée athmoshéprique
+data$descr_athmo<-factor(data$descr_athmo)
+data$descr_athmo<-as.numeric(data$descr_athmo)
 
 
 ##changement de donnée pour lumière
-for (i in 1:73643){
-  if(data$descr_lum[i]=="CrÃ©puscule ou aube"){
-    data$descr_lum[i] <- 0;
-  }
-  if(data$descr_lum[i]=="Nuit avec Ã©clairage public allumÃ©"){
-    data$descr_lum[i] <- 1;
-  }
-  if(data$descr_lum[i]=="Nuit sans Ã©clairage public"){
-    data$descr_lum[i] <- 2;
-  }
-  if(data$descr_lum[i]=="Plein jour"){
-    data$descr_lum[i] <- 3;
-  }
-  if(data$descr_lum[i]=="Nuit avec Ã©clairage public non allumÃ©"){
-    data$descr_lum[i] <- 4;
-  }
+data$descr_lum<-factor(data$descr_lum)
+data$descr_lum<-as.numeric(data$descr_lum)
 
-}
 
 ##changement de donnée pour létat de la surface
+data$descr_etat_surf<-factor(data$descr_etat_surf)
+data$descr_etat_surf<-as.numeric(data$descr_etat_surf)
+
+
+##changement de donnée pour intersection
+data$description_intersection<-factor(data$description_intersection)
+data$description_intersection<-as.numeric(data$description_intersection)
+
+
+##changement de donnée pour le dispositif de sécurité
+data$descr_dispo_secu<-factor(data$descr_dispo_secu)
+data$descr_dispo_secu<-as.numeric(data$descr_dispo_secu)
+
+
+#changement de donnée pour la descritpion de la personne après l'accident
+data$descr_grav<-factor(data$descr_grav)
+data$descr_grav<-as.numeric(data$descr_grav)
+
+
+##changement de donnée pour le motif du trajet
+data$descr_motif_traj<-factor(data$descr_motif_traj)
+data$descr_motif_traj<-as.numeric(data$descr_motif_traj)
+
+
+##changement de donnée pour le type de collision
+data$descr_type_col<-factor(data$descr_type_col)
+data$descr_type_col<-as.numeric(data$descr_type_col)
+
+
+#Changement de latitude et longitude pour les arrondissement de Marseille et Paris 
+
 for (i in 1:73643){
-  if(data$descr_etat_surf[i]=="Autre"){
-    data$descr_etat_surf[i] <- 0;
+  #Lyon
+  if(data$ville[i]=="LYON 01"){
+    data$latitude[i] <- 45.77039337158203;
+    data$longitude[i] <- 4.826514720916748;
   }
-  if(data$descr_etat_surf[i]=="EnneigÃ©e"){
-    data$descr_etat_surf[i] <- 1;
+  if(data$ville[i]=="LYON 02"){
+    data$latitude[i] <- 45.751625061035156;
+    data$longitude[i] <- 4.828275680541992;
   }
-  if(data$descr_etat_surf[i]=="Flaques"){
-    data$descr_etat_surf[i] <- 2;
+  if(data$ville[i]=="LYON 03"){
+    data$latitude[i] <- 45.7518424987793;
+    data$longitude[i] <- 4.876522064208984;
   }
-  if(data$descr_etat_surf[i]=="InondÃ©e"){
-    data$descr_etat_surf[i] <- 3;
+  if(data$ville[i]=="LYON 04"){
+    data$latitude[i] <- 45.77958679199219;
+    data$longitude[i] <- 4.825376987457275;
   }
-  if(data$descr_etat_surf[i]=="MouillÃ©e"){
-    data$descr_etat_surf[i] <- 4;
+  if(data$ville[i]=="LYON 05"){
+    data$latitude[i] <- 45.75631332397461;
+    data$longitude[i] <- 4.803836345672607;
   }
-  if(data$descr_etat_surf[i]=="Normale"){
-    data$descr_etat_surf[i] <- 5;
+  if(data$ville[i]=="LYON 06"){
+    data$latitude[i] <- 45.773250579833984;
+    data$longitude[i] <- 4.85155725479126;
   }
-  if(data$descr_etat_surf[i]=="VerglacÃ©e"){
-    data$descr_etat_surf[i] <- 6;
+  if(data$ville[i]=="LYON 07"){
+    data$latitude[i] <- 45.73286056518555;
+    data$longitude[i] <- 4.839175224304199;
   }
-  if(data$descr_etat_surf[i]=="Corps gras â€“ huile"){
-    data$descr_etat_surf[i] <- 7;
+  if(data$ville[i]=="LYON 08"){
+    data$latitude[i] <- 45.741737365722656;
+    data$longitude[i] <- 4.874378204345703;
+  }
+  if(data$ville[i]=="LYON 09"){
+    data$latitude[i] <- 45.775726318359375;
+    data$longitude[i] <- 4.801545143127441;
   }
   
-}
-
-##changement de donnée pour intersection 
-for (i in 1:73643){
-  if(data$description_intersection[i]=="Autre intersection"){
-    data$description_intersection[i] <- 0;
+  #Marseille
+  if(data$ville[i]=="MARSEILLE 01"){
+    data$latitude[i] <- 43.298004150390625;
+    data$longitude[i] <- 5.381344795227051;
   }
-  if(data$description_intersection[i]=="Giratoire"){
-    data$description_intersection[i] <- 1;
+  if(data$ville[i]=="MARSEILLE 02"){
+    data$latitude[i] <- 43.32648849487305;
+    data$longitude[i] <- 5.357941627502441;
   }
-  if(data$description_intersection[i]=="Hors intersection"){
-    data$description_intersection[i] <- 2;
+  if(data$ville[i]=="MARSEILLE 03"){
+    data$latitude[i] <- 43.309749603271484;
+    data$longitude[i] <- 5.379581928253174;
   }
-  if(data$description_intersection[i]=="Intersection à plus de 4 branches"){
-    data$description_intersection[i] <- 3;
+  if(data$ville[i]=="MARSEILLE 04"){
+    data$latitude[i] <- 43.306800842285156;
+    data$longitude[i] <- 5.399689674377441;
   }
-  if(data$description_intersection[i]=="Intersection en T"){
-    data$description_intersection[i] <- 4;
+  if(data$ville[i]=="MARSEILLE 05"){
+    data$latitude[i] <- 43.292266845703125;
+    data$longitude[i] <- 5.395776271820068;
   }
-  if(data$description_intersection[i]=="Intersection en X"){
-    data$description_intersection[i] <- 5;
+  if(data$ville[i]=="MARSEILLE 06"){
+    data$latitude[i] <- 43.28824234008789;
+    data$longitude[i] <- 5.380294322967529;
   }
-  if(data$description_intersection[i]=="Intersection en Y"){
-    data$description_intersection[i] <- 6;
+  if(data$ville[i]=="MARSEILLE 07"){
+    data$latitude[i] <- 43.28508377075195;
+    data$longitude[i] <- 5.360000133514404;
   }
-  if(data$description_intersection[i]=="Passage à  niveau"){
-    data$description_intersection[i] <- 7;
+  if(data$ville[i]=="MARSEILLE 08"){
+    data$latitude[i] <- 43.26696014404297;
+    data$longitude[i] <- 5.389336585998535;
   }
-  if(data$description_intersection[i]=="Place"){
-    data$description_intersection[i] <- 8;
+  if(data$ville[i]=="MARSEILLE 09"){
+    data$latitude[i] <- 43.26696014404297;
+    data$longitude[i] <- 5.389336585998535;
   }
-}
-
-##changement de donnée pour le dispositif de sécurité 
-for (i in 1:73643){
-  if(data$descr_dispo_secu[i]=="Autre - Non dÃ©terminable"){
-    data$descr_dispo_secu[i] <- 0;
+  if(data$ville[i]=="MARSEILLE 10"){
+    data$latitude[i] <- 43.26696014404297;
+    data$longitude[i] <- 5.389336585998535;
   }
-  if(data$descr_dispo_secu[i]=="PrÃ©sence d'un casque - Utilisation non dÃ©terminable"){
-    data$descr_dispo_secu[i] <- 1;
+  if(data$ville[i]=="MARSEILLE 11"){
+    data$latitude[i] <- 43.26696014404297;
+    data$longitude[i] <- 5.389336585998535;
   }
-  if(data$descr_dispo_secu[i]=="Autre - UtilisÃ©"){
-    data$descr_dispo_secu[i] <- 2;
+  if(data$ville[i]=="MARSEILLE 12"){
+    data$latitude[i] <- 43.26696014404297;
+    data$longitude[i] <- 5.389336585998535;
   }
-  if(data$descr_dispo_secu[i]=="Autre - Non utilisÃ©"){
-    data$descr_dispo_secu[i] <- 3;
+  if(data$ville[i]=="MARSEILLE 13"){
+    data$latitude[i] <- 43.26696014404297;
+    data$longitude[i] <- 5.389336585998535;
   }
-  if(data$descr_dispo_secu[i]=="PrÃ©sence d'un casque non utilisÃ©"){
-    data$descr_dispo_secu[i] <- 4;
+  if(data$ville[i]=="MARSEILLE 14"){
+    data$latitude[i] <- 43.26696014404297;
+    data$longitude[i] <- 5.389336585998535;
   }
-  if(data$descr_dispo_secu[i]=="PrÃ©sence d'une ceinture de sÃ©curitÃ© - Utilisation non dÃ©terminable"){
-    data$descr_dispo_secu[i] <- 5;
+  if(data$ville[i]=="MARSEILLE 15"){
+    data$latitude[i] <- 43.26696014404297;
+    data$longitude[i] <- 5.389336585998535;
   }
-  if(data$descr_dispo_secu[i]=="Utilisation d'un casque"){
-    data$descr_dispo_secu[i] <- 6;
+  if(data$ville[i]=="MARSEILLE 16"){
+    data$latitude[i] <- 43.26696014404297;
+    data$longitude[i] <- 5.389336585998535;
   }
-  if(data$descr_dispo_secu[i]=="PrÃ©sence de ceinture de sÃ©curitÃ© non utilisÃ©e"){
-    data$descr_dispo_secu[i] <- 7;
+  #Paris
+  if(data$ville[i]=="Paris 01"){
+    data$latitude[i] <- 48.8588897;
+    data$longitude[i] <- 2.320041;
   }
-  if(data$descr_dispo_secu[i]=="Utilisation d'un Ã©quipement rÃ©flÃ©chissant"){
-    data$descr_dispo_secu[i] <- 8;
+  if(data$ville[i]=="Paris 02"){
+    data$latitude[i] <- 48.8588897;
+    data$longitude[i] <- 2.320041;
   }
-  if(data$descr_dispo_secu[i]=="Utilisation d'une ceinture de sÃ©curitÃ©"){
-    data$descr_dispo_secu[i] <- 9;
+  if(data$ville[i]=="Paris 03"){
+    data$latitude[i] <- 48.8588897;
+    data$longitude[i] <- 2.320041;
   }
-  if(data$descr_dispo_secu[i]=="PrÃ©sence dispositif enfant - Utilisation non dÃ©terminable"){
-    data$descr_dispo_secu[i] <- 10;
+  if(data$ville[i]=="Paris 04"){
+    data$latitude[i] <- 48.8588897;
+    data$longitude[i] <- 2.320041;
   }
-  if(data$descr_dispo_secu[i]=="Utilisation d'un dispositif enfant"){
-    data$descr_dispo_secu[i] <- 11;
+  if(data$ville[i]=="Paris 05"){
+    data$latitude[i] <- 48.8588897;
+    data$longitude[i] <- 2.320041;
   }
-  if(data$descr_dispo_secu[i]==" PrÃ©sence d'un Ã©quipement rÃ©flÃ©chissant non utilisÃ©"){
-    data$descr_dispo_secu[i] <- 12;
+  if(data$ville[i]=="Paris 06"){
+    data$latitude[i] <- 48.8588897;
+    data$longitude[i] <- 2.320041;
   }
- 
-}
-
-##changement de donnée pour le motif du trajet 
-for (i in 1:73643){
-  if(data$descr_motif_traj[i]=="Autre"){
-    data$descr_motif_traj[i] <- 0;
+  if(data$ville[i]=="Paris 07"){
+    data$latitude[i] <- 48.8588897;
+    data$longitude[i] <- 2.320041;
   }
-  if(data$descr_motif_traj[i]=="Courses â€“ achats"){
-    data$descr_motif_traj[i] <- 1;
+  if(data$ville[i]=="Paris 08"){
+    data$latitude[i] <- 48.8588897;
+    data$longitude[i] <- 2.320041;
   }
-  if(data$descr_motif_traj[i]=="Domicile â€“ travail"){
-    data$descr_motif_traj[i] <- 2;
+  if(data$ville[i]=="Paris 09"){
+    data$latitude[i] <- 48.8588897;
+    data$longitude[i] <- 2.320041;
   }
-  if(data$descr_motif_traj[i]=="Domicile â€“ Ã©cole"){
-    data$descr_motif_traj[i] <- 3;
+  if(data$ville[i]=="Paris 10"){
+    data$latitude[i] <- 48.8588897;
+    data$longitude[i] <- 2.320041;
   }
-  if(data$descr_motif_traj[i]=="Promenade â€“ loisirs"){
-    data$descr_motif_traj[i] <- 4;
+  if(data$ville[i]=="Paris 11"){
+    data$latitude[i] <- 48.8588897;
+    data$longitude[i] <- 2.320041;
   }
-  if(data$descr_motif_traj[i]=="Utilisation professionnelle"){
-    data$descr_motif_traj[i] <- 5;
+  if(data$ville[i]=="Paris 12"){
+    data$latitude[i] <- 48.8588897;
+    data$longitude[i] <- 2.320041;
   }
-}
-
-##changement de donnée pour le type de collision 
-for (i in 1:73643){
-  if(data$descr_type_col[i]=="Deux vÃ©hicules - Frontale"){
-    data$descr_type_col[i] <- 0;
+  if(data$ville[i]=="Paris 13"){
+    data$latitude[i] <- 48.8588897;
+    data$longitude[i] <- 2.320041;
   }
-  if(data$descr_type_col[i]=="Deux vÃ©hicules â€“ Par lâ€™arriÃ¨re"){
-    data$descr_type_col[i] <- 1;
+  if(data$ville[i]=="Paris 14"){
+    data$latitude[i] <- 48.8588897;
+    data$longitude[i] <- 2.320041;
   }
-  if(data$descr_type_col[i]=="Deux vÃ©hicules â€“ Par le cotÃ©"){
-    data$descr_type_col[i] <- 2;
+  if(data$ville[i]=="Paris 15"){
+    data$latitude[i] <- 48.8588897;
+    data$longitude[i] <- 2.320041;
   }
-  if(data$descr_type_col[i]=="Sans collision"){
-    data$descr_type_col[i] <- 3;
+  if(data$ville[i]=="Paris 16"){
+    data$latitude[i] <- 48.8588897;
+    data$longitude[i] <- 2.320041;
   }
-  if(data$descr_type_col[i]=="Trois vÃ©hicules et plus â€“ Collisions multiples"){
-    data$descr_type_col[i] <- 4;
+  if(data$ville[i]=="Paris 17"){
+    data$latitude[i] <- 48.8588897;
+    data$longitude[i] <- 2.320041;
   }
-  if(data$descr_type_col[i]=="Trois vÃ©hicules et plus â€“ En chaÃ®ne"){
-    data$descr_type_col[i] <- 5;
+  if(data$ville[i]=="Paris 18"){
+    data$latitude[i] <- 48.8588897;
+    data$longitude[i] <- 2.320041;
   }
+  if(data$ville[i]=="Paris 19"){
+    data$latitude[i] <- 48.8588897;
+    data$longitude[i] <- 2.320041;
+  }
+  if(data$ville[i]=="Paris 20"){
+    data$latitude[i] <- 48.8588897;
+    data$longitude[i] <- 2.320041;
+  }
+  
 }
 
 # Supprimer les lignes contenant des valeurs manquantes
 data_sans_nan <- na.omit(data)
 
+
 # Supprimer les lignes contenant des valeurs NULL
 data_sans_nan_et_null <- data_sans_nan[complete.cases(data_sans_nan), ]
 
-# Afficher le nouveau tableau sans les valeurs manquantes et les valeurs NULL
-print(data_sans_nan_et_null)
-
+#ici je viens mettre cette ligne pour permettre la modification du tableau dans rstudio 
+stat_acc_V3=data 
 
 
